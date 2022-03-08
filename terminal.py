fa6430c49504be55b5174b73dd9156313fd69911
Sajad Ebrahimi <sajad.ebrahimi1378@gmail.com>
2022-03-08 15:26:07 +0330

    Fix bug in terminal & add new words

--- terminal.py
+++ terminal.py
@@ -1,3 +1,6 @@
+from finder import word_finder
+
+
 def run_terminal():
     game_mode = input('Enter game mode: 1 for vaajoor, 2 for wordle: ')
     if game_mode == '1':
@@ -5,10 +8,10 @@ def run_terminal():
     elif game_mode == '2':
         game_mode = 'wordle'
 
-    number_of_exact_input = int(input('Enter number of exact input: '))
+    number_of_input = int(input('Enter number of exact input: '))
     print('Enter each index and character in separate lines with space: ')
     exact_dict = {}
-    for i in range(number_of_exact_input):
+    for i in range(number_of_input):
         input_data = input().split()
         exact_dict[int(input_data[0]) - 1] = input_data[1]
 
@@ -16,10 +19,10 @@ def run_terminal():
             print('Error: index must be between 1 and 5')
             exit()
 
-    number_of_exact_input = int(input('Enter number of contain input: '))
+    number_of_input = int(input('Enter number of contain input: '))
     print('Enter each character in separate line: ')
     contains_list = []
-    for i in range(number_of_exact_input):
+    for i in range(number_of_input):
         contains_list.append(input())
 
     print(word_finder(game_mode, exact_dict, contains_list))
